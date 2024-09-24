b39087eed8612bb9efbd71a0e0c93c9d8abeb415
666zuo666 <165533320+666zuo666@users.noreply.github.com>
2024-09-24 16:09:56 +0800

    Update your_script.py

--- your_script.py
+++ your_script.py
@@ -47,8 +47,8 @@ if st.button("Predict"):
 
     # 绘图
     shap.initjs()  # 初始化 SHAP
-    shap.force_plot(explainer_shap.expected_value[predicted_class], shap_values[predicted_class], pd.DataFrame([feature_values], columns=feature_names))
-
+    %shap.force_plot(explainer_shap.expected_value[predicted_class], shap_values[predicted_class], pd.DataFrame([feature_values], columns=feature_names))
+    shap.force_plot(explainer_shap.expected_value[0], shap_values[:,:,0], pd.DataFrame([feature_values], columns=feature_names), matplotlib=True)
     # LIME 解释
     st.subheader("LIME Explanation")
     lime_explainer = LimeTabularExplainer(X_test.values, feature_names=feature_names, class_names=['LPA', 'Others'], mode='classification')
